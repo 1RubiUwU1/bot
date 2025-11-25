d2647c4281e19cfc3db32c31867203ad5f9c49be
Bi <110574193+1RubiUwU1@users.noreply.github.com>
2025-11-25 00:08:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
@@ -165,4 +167,5 @@ def cuentas():
 # -----------------------
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
 
