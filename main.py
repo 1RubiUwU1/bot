8b0afc03809bc031c8e5d23791e57b3066ad9cf5
Bi <110574193+1RubiUwU1@users.noreply.github.com>
2025-11-25 00:28:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -166,7 +166,8 @@ def cuentas():
 # Inicia el servidor
 # -----------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8080)
+
 
 
 
